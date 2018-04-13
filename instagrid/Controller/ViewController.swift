2422c9950a091271c6a9ad7b26aae5dfa3311a2d
Chrisdd3365 <32330136+Chrisdd3365@users.noreply.github.com>
2018-04-13 13:22:39 +0200

    new commit
    implementing programmatically the step 3 of instagrid: accessing photo library with tags on button, 1 picker controller

--- instagrid/Controller/ViewController.swift
+++ instagrid/Controller/ViewController.swift
@@ -8,10 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var topLeftImageView: UIImageView!
+    @IBOutlet weak var topRightImageView: UIImageView!
+    @IBOutlet weak var bottomLeftImageView: UIImageView!
+    @IBOutlet weak var bottomRightImageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    var imagePicked = 0
+    
+    @IBAction func chooseImage(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.savedPhotosAlbum){
+            imagePicked = sender.tag
+            present(imagePicker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        
+        if imagePicked == 1 {
+            topLeftImageView.image = pickedImage
+        }
+        if imagePicked == 2 {
+            topRightImageView.image = pickedImage
+        }
+        if imagePicked == 3 {
+            bottomLeftImageView.image = pickedImage
+        }
+        if imagePicked == 4 {
+            bottomRightImageView.image = pickedImage
+        }
+        dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
     }
 
     override func didReceiveMemoryWarning() {
